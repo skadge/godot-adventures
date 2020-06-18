f23330dd67b1cb46010d515cc685e0fc9f4ee224
Séverin Lemaignan <severin@guakamole.org>
2020-06-18 16:29:04 +0100

    Initial 'home made' game translation to French

--- res/interface/Dialogues.gd
+++ res/interface/Dialogues.gd
@@ -2,7 +2,24 @@ extends Control
 
 signal conversation_finished
 
+var strings_file
+var strings = {}
+
+func _init():
+    strings_file = File.new()
+    
+    
 func _ready():
+    
+    strings_file.open("res://chronicles_purdownia_translations.csv", File.READ)
+    
+    while not strings_file.eof_reached():
+        var line = strings_file.get_csv_line()
+        if line.size() > 1:
+            strings[line[0]] = line[1]
+
+    strings_file.close()
+        
     hide_all_buttons()
 
 
@@ -29,7 +46,7 @@ func say(sprite, text):
     hide_all_buttons()
     
     $Box/CharacterIcon.texture = sprite.texture
-    $Box/RichTextLabel.bbcode_text = text
+    set_text(text)
 
 
 func say_many(sprite, sentences):
@@ -45,7 +62,7 @@ func say_many(sprite, sentences):
     
     for idx in range(sentences.size()):
         
-        $Box/RichTextLabel.bbcode_text = sentences[idx]
+        set_text(sentences[idx])
         
         if idx < sentences.size() - 1:
             yield(btnContinue, "pressed")
@@ -54,6 +71,15 @@ func say_many(sprite, sentences):
             get_parent().set_process_input(false)
             emit_signal("conversation_finished")
 
+
+func set_text(text):
+
+    if strings.has(text):
+        text = strings[text]
+    else:
+        print("[MISSING STRING] " + text)
+        
+    $Box/RichTextLabel.bbcode_text = text
     
 func say_yes_no(sprite, text, cb_object, on_yes, on_no):
     
@@ -65,7 +91,7 @@ func say_yes_no(sprite, text, cb_object, on_yes, on_no):
     $Box/Buttons/ButtonNo.show()
     
     $Box/CharacterIcon.texture = sprite.texture
-    $Box/RichTextLabel.bbcode_text = text
+    set_text(text)
         
     $Box/Buttons/ButtonYes.connect("pressed", cb_object, on_yes, [], CONNECT_ONESHOT)
     $Box/Buttons/ButtonNo.connect("pressed", cb_object, on_no, [], CONNECT_ONESHOT)
