a1819702a5bb9d526594ebd954d0acd3e3ba5a97
Séverin Lemaignan <severin@guakamole.org>
2020-06-21 13:35:26 +0100

    Added final dialogue to wiseman

--- res/characters/wiseman.gd
+++ res/characters/wiseman.gd
@@ -10,7 +10,14 @@ func dialogue_stages():
         sentences +=["I am the chief of this village"]
         is_first_time = false
 
+    if player.golden_pineapple_available:
+        sentences = ["You brought back the mythical Golden Pineapple!",
+                     "I had heard of this magical fruit, but I thought it was a legend!",
+                     "You are a hero!!",
+                     "This will cure the village, no doubt about it!",
+                     "Thank you so much!!"]
+    else:
         sentences += ["people in the village are feeling sick.",
-                      "Help us find a cure.When you find it bring it to me!",
-                      "The castle may know somthing about it."]
+                        "Help us find a cure.When you find it bring it to me!",
+                        "The castle may know something about it."]
     dialogue.say_many($Sprite, sentences) 
